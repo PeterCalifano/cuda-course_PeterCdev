fd1ad423496250905d595d6f585a3abe2cdcb149
PeterC <petercalifano.gs@gmail.com>
2025-04-01 08:45:22 +0200

    Add WS file

--- 02_Setup/cuda-installer.sh
+++ 02_Setup/cuda-installer.sh
@@ -1,5 +1,6 @@
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove
-wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
-sudo sh cuda_12.6.0_560.28.03_linux.run
-nvcc --version
-nvidia-smi
+#sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+#wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/#cuda_12.6.0_560.28.03_linux.run
+#sudo sh cuda_12.6.0_560.28.03_linux.run
+#nvcc --version
+#nvidia-smi
+echo "Do not use this script, install it manually."
